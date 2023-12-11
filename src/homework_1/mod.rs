ddb3e7ac4572085639c3655cafe7a16fd81f26cb
iiiokojiadbi <iiiokojiadbi@yandex.ru>
2023-12-12 00:16:46 +0300

    Добавляет проверки на переполнение, добавляет пограничные тесты

--- src/homework_1/mod.rs
+++ src/homework_1/mod.rs
@@ -1,33 +1,33 @@
-use std::usize;
-
-pub fn double_int32(&value: &u32) -> u32 {
-    value * 2
+pub fn double_int32(value: u32) -> u32 {
+    value.saturating_mul(2)
 }
 
-pub fn double_int64(&value: &u32) -> u64 {
-    (value * 2) as u64
+pub fn double_int64(value: u32) -> u64 {
+    (value as u64).saturating_mul(2)
 }
 
-pub fn double_float32(&value: &f32) -> f32 {
-    value * 2.0
+pub fn double_float32(value: f32) -> f32 {
+    value + value
 }
 
-pub fn double_float64(&value: &f32) -> f64 {
-    (value * 2.0) as f64
+pub fn double_float64(value: f32) -> f64 {
+    (value as f64) + (value as f64)
 }
 
-pub fn int_plus_float_to_float(&int_value: &u32, &float_value: &f32) -> f64 {
-    (int_value as f32 + float_value) as f64
+pub fn int_plus_float_to_float(int_value: u32, float_value: f32) -> f64 {
+    (int_value as f64) + (float_value as f64)
 }
 
-pub fn int_plus_float_to_int(&int_value: &u32, &float_value: &f32) -> u64 {
-    (int_value as f32 + float_value) as u64
+pub fn int_plus_float_to_int(int_value: u32, float_value: f32) -> u64 {
+    (int_value as f32 + float_value).round() as u64
 }
 
-pub fn tuple_sum(values: &(usize, usize)) -> usize {
-    values.0 + values.1
+pub fn tuple_sum(values: (usize, usize)) -> usize {
+    (values.0).saturating_add(values.1)
 }
 
-pub fn array_sum(arr: &[usize; 3]) -> usize {
-    arr.iter().sum()
+pub fn array_sum(arr: [usize; 3]) -> usize {
+    arr.into_iter()
+        .reduce(|acc, value| acc.saturating_add(value))
+        .unwrap()
 }
--- tests/homework_1/mod.rs
+++ tests/homework_1/mod.rs
@@ -2,64 +2,71 @@ use rust_learn::homework_1;
 
 #[test]
 fn double_int32() {
-    let test_value = 5;
-
-    assert_eq!(homework_1::double_int32(&test_value), 10);
+    assert_eq!(homework_1::double_int32(0), 0);
+    assert_eq!(homework_1::double_int32(5), 10);
+    assert_eq!(homework_1::double_int32(3_000_000_000), u32::MAX);
 }
 
 #[test]
 fn double_int64() {
-    let test_value = 25;
-
-    assert_eq!(homework_1::double_int64(&test_value), 50);
+    assert_eq!(homework_1::double_int64(0), 0);
+    assert_eq!(homework_1::double_int64(25), 50);
+    assert_eq!(homework_1::double_int64(3_000_000_000), 6_000_000_000);
 }
 
 #[test]
 fn double_float32() {
-    let test_value = 10.0;
-
-    assert_eq!(homework_1::double_float32(&test_value), 20.0);
+    assert_eq!(homework_1::double_float32(0.0), 0.0);
+    assert_eq!(homework_1::double_float32(10.0), 20.0);
+    assert_eq!(homework_1::double_float32(f32::MAX), f32::INFINITY);
 }
 
 #[test]
 fn double_float64() {
-    let test_value = 10.0;
-
-    assert_eq!(homework_1::double_float64(&test_value), 20.0);
+    assert_eq!(homework_1::double_float64(0.0), 0.0);
+    assert_eq!(homework_1::double_float64(10.0), 20.0);
+    assert_eq!(homework_1::double_float64(10.0), 20.0);
+    assert!(homework_1::double_float64(f32::MAX) <= f64::MAX)
 }
 
 #[test]
 fn int_plus_float_to_float() {
-    let test_int_value = 10;
-    let test_float_value = 10.0;
-
-    assert_eq!(
-        homework_1::int_plus_float_to_float(&test_int_value, &test_float_value),
-        20.0
-    );
+    assert_eq!(homework_1::int_plus_float_to_float(0, 0.0), 0.0);
+    assert_eq!(homework_1::int_plus_float_to_float(10, 10.0), 20.0);
+    assert!(homework_1::int_plus_float_to_float(u32::MAX, f32::MAX) <= f64::MAX);
 }
 
 #[test]
 fn int_plus_float_to_int() {
-    let test_int_value = 10;
-    let test_float_value = 10.0;
-
+    assert_eq!(homework_1::int_plus_float_to_int(0, 0.0), 0);
+    assert_eq!(homework_1::int_plus_float_to_int(10, 10.0), 20);
     assert_eq!(
-        homework_1::int_plus_float_to_int(&test_int_value, &test_float_value),
-        20
+        homework_1::int_plus_float_to_int(u32::MAX, f32::MAX),
+        u64::MAX
+    );
+    assert_eq!(
+        homework_1::int_plus_float_to_int(u32::MIN, f32::MIN),
+        u64::MIN
     );
 }
 
 #[test]
 fn tuple_sum() {
-    let values = (5, 1);
-
-    assert_eq!(homework_1::tuple_sum(&values), 6);
+    assert_eq!(homework_1::tuple_sum((0, 0)), 0);
+    assert_eq!(homework_1::tuple_sum((5, 1)), 6);
+    assert_eq!(homework_1::tuple_sum((1, usize::MAX)), usize::MAX);
+    assert!(homework_1::tuple_sum((1, usize::MIN)) > usize::MIN);
+    assert_eq!(homework_1::tuple_sum((usize::MIN, usize::MIN)), usize::MIN);
 }
 
 #[test]
 fn array_sum() {
-    let values = [1, 2, 3];
-
-    assert_eq!(homework_1::array_sum(&values), 6);
+    assert_eq!(homework_1::array_sum([0, 0, 0]), 0);
+    assert_eq!(homework_1::array_sum([1, 2, 3]), 6);
+    assert_eq!(
+        homework_1::array_sum([1, usize::MIN, usize::MAX]),
+        usize::MAX
+    );
+    assert_eq!(homework_1::array_sum([usize::MAX; 3]), usize::MAX);
+    assert_eq!(homework_1::array_sum([usize::MIN; 3]), usize::MIN);
 }
