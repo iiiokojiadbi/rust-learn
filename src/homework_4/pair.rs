b78d5478f07fc8215335a86bd98dd21de862b152
iiiokojiadbi <iiiokojiadbi@yandex.ru>
2024-01-25 13:42:09 +0300

    Выполняет часть 4 задания

--- src/homework_4/pair.rs
+++ src/homework_4/pair.rs
@@ -2,11 +2,11 @@
 pub struct Pair(i32, i32);
 
 impl Pair {
-    pub fn vector_sum(&self, rhs: &Pair) -> Self {
+    pub fn vector_sum(&self, rhs: Pair) -> Self {
         Self(self.0 + rhs.0, self.1 + rhs.1)
     }
 
-    pub fn scalar_sum(&self, rhs: &Pair) -> i32 {
+    pub fn scalar_sum(&self, rhs: Pair) -> i32 {
         self.0 + self.1 + rhs.0 + rhs.1
     }
 }
@@ -23,24 +23,26 @@ mod test {
     }
 
     #[test]
-    fn test_vector_sum() {
+    fn test_vector_sum_pair() {
         let pair = Pair(1, 1);
+        let rhs = Pair(1, 1);
         let expected = Pair(2, 2);
-        assert_eq!(pair.vector_sum(&pair), expected);
+        assert_eq!(pair.vector_sum(rhs), expected);
 
         let pair = Pair(2, 2);
+        let rhs = Pair(2, 2);
         let expected = Pair(4, 4);
-        assert_eq!(pair.vector_sum(&pair), expected);
+        assert_eq!(pair.vector_sum(rhs), expected);
     }
 
     #[test]
-    fn test_scalar_sum() {
+    fn test_scalar_sum_pair() {
         let pair = Pair(1, 1);
-        let expected = Pair(2, 2);
-        assert_eq!(pair.scalar_sum(&expected), 6);
+        let rhs: Pair = Pair(2, 2);
+        assert_eq!(pair.scalar_sum(rhs), 6);
 
         let pair = Pair(2, 1);
-        let expected = Pair(4, 2);
-        assert_eq!(pair.scalar_sum(&expected), 9);
+        let rhs = Pair(4, 2);
+        assert_eq!(pair.scalar_sum(rhs), 9);
     }
 }
