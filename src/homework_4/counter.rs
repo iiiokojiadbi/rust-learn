21f85881001e194ffbf0f34d5ab8ba8438207b31
iiiokojiadbi <iiiokojiadbi@yandex.ru>
2024-01-25 23:31:23 +0300

    Выполняет домашнее задание

--- src/homework_4/counter.rs
+++ src/homework_4/counter.rs
@@ -1,64 +1,94 @@
-type SignedCounter = isize;
-type UnsignedCounter = usize;
+#[derive(Default, Debug, PartialEq)]
+pub struct SignedCounter(isize);
 
-pub fn default_signed_counter() -> SignedCounter {
-    0
+#[derive(Default, Debug, PartialEq)]
+pub struct UnsignedCounter(usize);
+
+impl SignedCounter {
+    pub fn prev_value(&mut self) -> &Self {
+        self.0 -= 1;
+
+        self
+    }
+
+    pub fn next_value(&mut self) -> &Self {
+        self.0 += 1;
+
+        self
+    }
+
+    pub fn value(&self) -> isize {
+        self.0
+    }
 }
 
-pub fn default_unsigned_counter() -> UnsignedCounter {
-    0
-}
+impl UnsignedCounter {
+    pub fn next_value(&mut self) -> &Self {
+        self.0 += 1;
 
-pub fn next_signed(counter: SignedCounter) -> SignedCounter {
-    counter + 1
-}
+        self
+    }
 
-pub fn next_unsigned(counter: UnsignedCounter) -> UnsignedCounter {
-    counter + 1
-}
-
-pub fn prev_signed(counter: SignedCounter) -> SignedCounter {
-    counter - 1
+    pub fn value(&self) -> usize {
+        self.0
+    }
 }
 
 #[cfg(test)]
 mod test {
-    use super::*;
+
+    use super::{SignedCounter, UnsignedCounter};
 
     #[test]
     fn test_default_signed_counter() {
-        assert_eq!(default_signed_counter(), 0);
+        let signed_counter = SignedCounter::default();
+        let expected = SignedCounter(0);
+        assert_eq!(signed_counter, expected);
     }
 
     #[test]
     fn test_default_unsigned_counter() {
-        assert_eq!(default_unsigned_counter(), 0);
+        let unsigned_counter = UnsignedCounter::default();
+        let expected = UnsignedCounter(0);
+        assert_eq!(unsigned_counter, expected);
     }
 
     #[test]
     fn test_next_signed() {
-        assert_eq!(next_signed(-1), 0);
-        assert_eq!(next_signed(1), 2);
-        assert_eq!(next_signed(-2), -1);
+        let mut signed_counter = SignedCounter::default();
 
-        assert_ne!(next_signed(-1), -1);
+        signed_counter.next_value();
+        let expected = SignedCounter(1);
+        assert_eq!(signed_counter.value(), expected.value());
+
+        signed_counter.next_value();
+        let expected = SignedCounter(2);
+        assert_eq!(signed_counter.value(), expected.value());
     }
 
     #[test]
     fn test_next_unsigned() {
-        assert_eq!(next_unsigned(4), 5);
-        assert_eq!(next_unsigned(0), 1);
+        let mut unsigned_counter = UnsignedCounter::default();
 
-        assert_ne!(next_unsigned(0), 0);
-        assert_ne!(next_unsigned(0), 2);
+        unsigned_counter.next_value();
+        let expected = UnsignedCounter(1);
+        assert_eq!(unsigned_counter.value(), expected.value());
+
+        unsigned_counter.next_value();
+        let expected = UnsignedCounter(2);
+        assert_eq!(unsigned_counter.value(), expected.value());
     }
 
     #[test]
     fn test_prev_signed() {
-        assert_eq!(prev_signed(4), 3);
-        assert_eq!(prev_signed(0), -1);
+        let mut signed_counter = SignedCounter::default();
 
-        assert_ne!(prev_signed(0), 1);
-        assert_ne!(prev_signed(0), 0);
+        signed_counter.prev_value();
+        let expected = SignedCounter(-1);
+        assert_eq!(signed_counter.value(), expected.value());
+
+        signed_counter.prev_value();
+        let expected = SignedCounter(-2);
+        assert_eq!(signed_counter.value(), expected.value());
     }
 }
